181a3a992b8a3a798ea05ff85bb0f7c68e44a093
Nakisax <114724195+Nakisax@users.noreply.github.com>
2022-09-29 23:27:20 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+int n1 , n2 , n3 , n4 , n5 , n6, n7 , n8 , n9;
+using namespace std;
+// check for command like + - etc
+void checker(int d)
+{ 
+
+
+
+
+}
+
+int main()
+{
+	cout << "Welcome to 9-digit Calculator\n";
+#define calculator
+	calculator
+	{
+		for (bool yes = true; yes; NULL)
+		{
+		
+			break;
+
+
+		}
+	}
+
+
+	string n = "Kaboom", k = "zug zug";
+	string d = n = k = "dabomb";
+	cout << d;
+
+
+}
